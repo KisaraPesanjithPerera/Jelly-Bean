89b5bb05818acdb221896f201a64f0a02edfc45f
Mᴀʀɪᴏ Yᴇʟʟᴏᴡʙᴇᴀʀᴅ シ︎ <marioyellowbeard@gmail.com>
2022-04-11 10:56:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -414,6 +414,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ],[
             InlineKeyboardButton('🧿 Help', callback_data='help'),
             InlineKeyboardButton('About ℹ️', callback_data='about')
+        ], [
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
